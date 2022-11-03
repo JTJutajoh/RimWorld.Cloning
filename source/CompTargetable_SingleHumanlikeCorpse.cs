0808c7c336d945433c963a550e43bee52f1450c6
Justin <darkflame57@gmail.com>
2022-11-02 17:32:56 -0700

    Fix Cloning Mech Serum can target animal corpses

---
+++ source/CompTargetable_SingleHumanlikeCorpse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace Dark.Cloning
+{
+    class CompTargetable_SingleHumanlikeCorpse : CompTargetable
+    {
+		protected override bool PlayerChoosesTarget => true;
+
+		protected override TargetingParameters GetTargetingParameters()
+		{
+			return new TargetingParameters
+			{
+				canTargetPawns = false,
+				canTargetBuildings = false,
+				canTargetItems = true,
+				mapObjectTargetsMustBeAutoAttackable = false,
+				validator = (TargetInfo x) => x.Thing is Corpse && x.Thing.HasThingCategory(ThingCategoryDefOf.CorpsesHumanlike) && BaseTargetValidator(x.Thing)
+			};
+		}
+
+		public override IEnumerable<Thing> GetTargets(Thing targetChosenByPlayer = null)
+		{
+			yield return targetChosenByPlayer;
+		}
+	}
+}
