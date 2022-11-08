73eece2527d913991e7235f61ddee81ccd604029
Justin <darkflame57@gmail.com>
2022-11-08 02:11:39 -0800

    Fix capitalization typo

--- source/AI/WorkGiver_CarryToCloneExtractor.cs
+++ source/AI/WorkGiver_CarryToCloneExtractor.cs
@@ -8,11 +8,11 @@ using Verse;
 
 namespace Dark.Cloning
 {
-	public class WorkGiver_CarryTocloneExtractor : WorkGiver_CarryToBuilding
+	public class WorkGiver_CarryToCloneExtractor : WorkGiver_CarryToBuilding
 	{
 		public override ThingRequest ThingRequest => ThingRequest.ForDef(CloneDefOf.CloneExtractor);
 
-		public override bool ShouldSkip(Pawn pawn, bool forced = false)
+        public override bool ShouldSkip(Pawn pawn, bool forced = false)
 		{
 			return !ModsConfig.BiotechActive;
 		}
