208a7f39cfe43d74cc9f239ca574e38bd997881d
Justin <darkflame57@gmail.com>
2022-11-08 07:55:21 -0800

    Embryo genes now show cloned xenogenes

--- source/Patches/Patch_GeneUIUtility_DrawGeneSections.cs
+++ source/Patches/Patch_GeneUIUtility_DrawGeneSections.cs
@@ -12,10 +12,19 @@ using RimWorld;
 
 namespace Dark.Cloning
 {
+    public delegate void DrawSectionDelegate(Rect rect, bool xeno, int count, ref float curY, ref float sectionHeight, Action<int, Rect> drawer, Rect containingRect);
+
     [HarmonyPatch(typeof(GeneUIUtility), "DrawGeneSections")]
-    class Patch_GeneUIUtility_DrawGeneSections
+    static class Patch_GeneUIUtility_DrawGeneSections
     {
         static MethodInfo anchorMethod = typeof(Widgets).GetMethod("EndScrollView");
+
+        static DrawSectionDelegate DrawSection = AccessTools.MethodDelegate<DrawSectionDelegate>(AccessTools.Method(typeof(GeneUIUtility), "DrawSection"));
+
+        // endogenesHeight because for some dumb reason vanilla uses xenogenes for embryos even though they're actually endogenes
+        //DrawGenesInfo just sums the two together so it shouldn't matter which is used here
+        static FieldInfo xenogenesHeight = AccessTools.Field(typeof(GeneUIUtility), "endogenesHeight");
+        static FieldInfo scrollHeight = AccessTools.Field(typeof(GeneUIUtility), "scrollHeight");
 
         static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
@@ -24,12 +33,14 @@ namespace Dark.Cloning
             {
                 if (codes[i].Calls(anchorMethod))
                 {
-                    yield return new CodeInstruction(OpCodes.Ldarg, 1); // Thing target
+                    yield return new CodeInstruction(OpCodes.Ldarg, 1) // Thing target
+                    {labels = codes[i].labels}; // Make sure this isn't included in the if block just before the anchor method call
                     yield return new CodeInstruction(OpCodes.Ldarg, 0); // Rect rect
                     yield return new CodeInstruction(OpCodes.Ldloc, 1); // float curY
                     yield return new CodeInstruction(OpCodes.Ldloc, 2); // Rect containingRect
 
                     yield return CodeInstruction.Call(typeof(Patch_GeneUIUtility_DrawGeneSections), "DrawXenogenesForEmbryo");
+                    codes[i].labels = new List<Label>();
                 }
                 yield return codes[i];
             }
@@ -49,38 +60,18 @@ namespace Dark.Cloning
                 // The target is a clone embryo, draw its xenogenes
                 List<GeneDef> xenogenes = cloneComp.cloneData.forcedXenogenes.GenesListForReading;
 
-                MethodInfo DrawSection = AccessTools.Method(typeof(GeneUIUtility), "DrawSection");
-
                 void drawer(int i, Rect r)
                 {
                     Log.Message($"Drawing genedef {xenogenes[i].LabelCap} at {r.x},{r.y} size {r.width},{r.height}");
-                    Widgets.Label(r, $"Gene: {xenogenes[i]}");
                     GeneUIUtility.DrawGeneDef(xenogenes[i], r, GeneType.Xenogene, null, true, true, false);
                 }
-                Action<int, Rect> drawAction = drawer;
 
-                FieldInfo xenogenesHeight = AccessTools.Field(typeof(GeneUIUtility), "endogenesHeight");
-
-                object[] args = new object[7]
-                {
-                    rect,
-                    true,
-                    xenogenes.Count,
-                    curY,
-                    xenogenesHeight.GetValue(null),
-                    drawAction,
-                    containingRect
-                };
-                DrawSection.Invoke(null, args);
-                // Update the values that were passed by ref
-                //curY
-                curY = (float)args[3];
-                //xenogenesHeight
-                xenogenesHeight.SetValue(null, args[4]);
+                float tmpXenogenesHeight = (float)xenogenesHeight.GetValue(null);
+                DrawSection(rect, true, xenogenes.Count, ref curY, ref tmpXenogenesHeight, drawer, containingRect);
+                xenogenesHeight.SetValue(null, tmpXenogenesHeight);
 
                 if (Event.current.type == EventType.Layout)
                 {
-                    FieldInfo scrollHeight = AccessTools.Field(typeof(GeneUIUtility), "scrollHeight");
                     scrollHeight.SetValue(null, curY);
                 }
             }
